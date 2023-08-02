163a24d5ea9ef88e6ea0b2b40d3ce775933e4c89
Елизавета Зуева <elizavetazueva@MacBook-Air-Elizaveta.local>
2023-08-02 17:17:03 +0300

    Add CinemaFragment

--- app/src/main/java/com/example/neobis_android_tour_guide/CinemaFragment.kt
+++ app/src/main/java/com/example/neobis_android_tour_guide/CinemaFragment.kt
@@ -5,29 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.neobis_android_tour_guide.databinding.FragmentCinemaBinding
 import com.example.neobis_android_tour_guide.databinding.FragmentFitnessBinding
+import com.example.neobis_android_tour_guide.databinding.FragmentRestaurantsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CinemaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CinemaFragment : Fragment() {
 
     private lateinit var binding: FragmentCinemaBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentCinemaBinding.inflate(layoutInflater)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = FragmentCinemaBinding.inflate(inflater, container, false)
+        binding.recyclerCinema.layoutManager = LinearLayoutManager(requireContext())
+        val list = arrayListOf(Place(R.drawable.photo_cinema_moscow, getString(R.string.name_cinema_moscow),
+            getString(R.string.address_cinema_moscow), getString(R.string.opening_cinema_moscow),
+            getString(R.string.km_cinema_moscow), getString(R.string.check_cinema_moscow),
+            getString(R.string.discription_cinema_moscow), getString(R.string.fullDiscription_cinema_moscow)),
+            Place(R.drawable.photo_cinema_october, getString(R.string.name_cinema_october),
+                getString(R.string.address_cinema_october), getString(R.string.opening_cinema_october),
+                getString(R.string.km_cinema_october), getString(R.string.check_cinema_october),
+                getString(R.string.discription_cinema_october), getString(R.string.fullDiscription_cinema_october)))
+        binding.recyclerCinema.adapter = RecyclerViewAdapter(list)
         return binding.root
     }
 }
